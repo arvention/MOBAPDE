48f6439a1508eb4378c95998b2a8ba318e5c397c
arvention <amc.antioquia@yahoo.com.ph>
2016-02-02 23:38:04 +0800

    List View - UI changes
    added avatars and UI for music

--- IntroApp_ListView/app/src/main/java/com/example/arces/introapp/StarkActivity.java
+++ IntroApp_ListView/app/src/main/java/com/example/arces/introapp/StarkActivity.java
@@ -1,7 +1,11 @@
 package com.example.arces.introapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
