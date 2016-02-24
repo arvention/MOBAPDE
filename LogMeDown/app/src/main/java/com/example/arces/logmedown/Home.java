e867cb6e7825e56f2817f7073c2df77747b3bb31
arvention <amc.antioquia@yahoo.com.ph>
2016-02-24 16:13:40 +0800

    LogMeDown - Home Alert Dialog Log out

--- LogMeDown/app/src/main/java/com/example/arces/logmedown/Home.java
+++ LogMeDown/app/src/main/java/com/example/arces/logmedown/Home.java
@@ -1,10 +1,12 @@
 package com.example.arces.logmedown;
 
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.app.AlertDialog;
 
 public class Home extends AppCompatActivity {
 
@@ -18,21 +20,47 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        buttonLogOut = (Button)findViewById(R.id.buttonLogOut);
+        buttonLogOut = (Button) findViewById(R.id.buttonLogOut);
         buttonLogOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sharedPreferences = getSharedPreferences(USERPREFERENCES, MODE_PRIVATE);
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.clear();
-                editor.commit();
-                finish();
+                new AlertDialog.Builder(Home.this)
+                        .setTitle("Log Out")
+                        .setMessage(R.string.textLogOutPrompt)
+                        .setPositiveButton(R.string.textLogOutYes, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                sharedPreferences = getSharedPreferences(USERPREFERENCES, MODE_PRIVATE);
+                                SharedPreferences.Editor editor = sharedPreferences.edit();
+                                editor.clear();
+                                editor.commit();
+                                finish();
+                            }
+
+                        })
+                        .setNegativeButton(R.string.textLogOutNo, null)
+                        .show();
             }
         });
     }
 
     @Override
-    public void onBackPressed(){
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setTitle("Log Out")
+                .setMessage(R.string.textLogOutPrompt)
+                .setPositiveButton(R.string.textLogOutYes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        sharedPreferences = getSharedPreferences(USERPREFERENCES, MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.clear();
+                        editor.commit();
+                        finish();
+                    }
 
+                })
+                .setNegativeButton(R.string.textLogOutNo, null)
+                .show();
     }
 }
