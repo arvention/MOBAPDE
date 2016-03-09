e605808b37cb89a519c3b4bd3a21d9c85a1d88ac
arvention <amc.antioquia@yahoo.com.ph>
2016-03-09 09:05:27 +0800

    Home Recycler View

--- LogMeDown/app/src/main/java/com/example/arces/logmedown/HomeFragment.java
+++ LogMeDown/app/src/main/java/com/example/arces/logmedown/HomeFragment.java
@@ -2,12 +2,26 @@ package com.example.arces.logmedown;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class HomeFragment extends Fragment {
+
+    private  NoteRecyclerAdapter noteRecyclerAdapter;
+    private RecyclerView recyclerView;
+    private User user;
+
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        return inflater.inflate(R.layout.tab_fragment_home, container, false);
+        View view = inflater.inflate(R.layout.tab_fragment_home, container, false);
+        user = (User) getActivity().getIntent().getSerializableExtra("logged_user");
+        noteRecyclerAdapter = new NoteRecyclerAdapter(user.getNotes());
+
+        recyclerView = (RecyclerView)view.findViewById(R.id.recycler_view_home);
+        recyclerView.setAdapter(noteRecyclerAdapter);
+
+        return view;
     }
 }
---
+++ LogMeDown/app/src/main/java/com/example/arces/logmedown/NoteRecyclerAdapter.java
@@ -0,0 +1,60 @@
+package com.example.arces.logmedown;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class NoteRecyclerAdapter extends RecyclerView.Adapter<NoteRecyclerAdapter.NoteViewHolder> {
+
+    private ArrayList<Note> notes;
+
+    public NoteRecyclerAdapter(ArrayList<Note> notes){
+        this.notes = notes;
+    }
+
+    @Override
+    public NoteViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_card_layout, parent,false);
+        NoteViewHolder noteViewHolder = new NoteViewHolder(view);
+        return  noteViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(NoteViewHolder holder, int position) {
+        holder.userName.setText(notes.get(position).getCreator().getFirstName() + " " + notes.get(position).getCreator().getLastName());
+        holder.userPostTitle.setText(notes.get(position).getTitle());
+        holder.userImage.setImageResource(R.drawable.profile_img);
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+    public static class NoteViewHolder extends RecyclerView.ViewHolder {
+        CardView cardView;
+        TextView userName;
+        TextView userPostTitle;
+        ImageView userImage;
+
+        public NoteViewHolder(View itemView) {
+            super(itemView);
+            cardView = (CardView) itemView.findViewById(R.id.card_layout);
+            userName = (TextView) itemView.findViewById(R.id.user_name);
+            userPostTitle = (TextView) itemView.findViewById(R.id.user_post_title);
+            userImage = (ImageView) itemView.findViewById(R.id.user_image);
+        }
+    }
+}
+
