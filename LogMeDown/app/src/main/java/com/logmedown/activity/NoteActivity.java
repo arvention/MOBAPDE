f261119b756e18760ea39de14db7bada54f03afc
ebackers <arcestalavera@gmail.com>
2016-03-13 01:58:55 +0800

    Changing of bloc in note - WIP

--- LogMeDown/app/src/main/java/com/logmedown/activity/NoteActivity.java
+++ LogMeDown/app/src/main/java/com/logmedown/activity/NoteActivity.java
@@ -1,20 +1,31 @@
 package com.logmedown.activity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.arces.logmedown.R;
 import com.logmedown.database.Database;
+import com.logmedown.model.Bloc;
 import com.logmedown.model.Note;
 import com.logmedown.model.User;
 
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 
 public class NoteActivity extends AppCompatActivity {
@@ -24,16 +35,20 @@ public class NoteActivity extends AppCompatActivity {
     private Database db;
     private String action;
     private Note editNote;
+    private ImageButton locationBtn;
+    private Bloc bloc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
 
+        //initialize bloc to null (public)
+        bloc = null;
+
         //retrieve note action
         action = getIntent().getStringExtra("note_action");
         Log.d("note_debug", action);
-
 
         //retrieve logged user details
         loggedUser = (User) getIntent().getSerializableExtra("logged_user");
@@ -61,16 +76,25 @@ public class NoteActivity extends AppCompatActivity {
         editTitle = (EditText) findViewById(R.id.addNoteTitle);
         editContent = (EditText) findViewById(R.id.addNoteContent);
 
-        if(action.equals("edit")){
-            editNote = (Note) getIntent().getSerializableExtra("note_details");
+        locationBtn = (ImageButton) findViewById(R.id.noteLocationButton);
 
+        locationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showLocationAlert(action);
+            }
+        });
+
+        if (action.equals("edit")) {
+            editNote = (Note) getIntent().getSerializableExtra("note_details");
+            bloc = editNote.getBloc();
             editTitle.setText(editNote.getTitle());
             editContent.setText(editNote.getContent());
         }
     }
 
     public void cancelNoteAction() {
-        if(action.equals("add")) {
+        if (action.equals("add")) {
             new AlertDialog.Builder(this)
                     .setTitle("Discard Note")
                     .setMessage(R.string.textSaveDiscardPrompt)
@@ -83,8 +107,7 @@ public class NoteActivity extends AppCompatActivity {
                     })
                     .setNegativeButton(R.string.textLogOutNo, null)
                     .show();
-        }
-        else if(action.equals("edit")){
+        } else if (action.equals("edit")) {
             new AlertDialog.Builder(this)
                     .setTitle("Discard Changes")
                     .setMessage(R.string.textEditDiscardPrompt)
@@ -128,8 +151,7 @@ public class NoteActivity extends AppCompatActivity {
                     })
                     .setNegativeButton(R.string.textCancel, null)
                     .show();
-        }
-        else if(action.equals("edit")){
+        } else if (action.equals("edit")) {
             new AlertDialog.Builder(this)
                     .setTitle("Edit Note")
                     .setMessage(R.string.textEditSavePrompt)
@@ -156,5 +178,48 @@ public class NoteActivity extends AppCompatActivity {
                     .setNegativeButton(R.string.textCancel, null)
                     .show();
         }
+    }
+
+    public void showLocationAlert(final String action) {
+        //ArrayList<Bloc> blocList = loggedUser.getBlocs();
+        ArrayList<String> blocList = new ArrayList<>();
+
+        //add public to list of choices
+        blocList.add(0, "Public");
+
+        //alert
+        AlertDialog.Builder locationAlert = new AlertDialog.Builder(this);
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View alertView = inflater.inflate(R.layout.note_location_alert, null);
+
+        //spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_spinner_item, blocList);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        final Spinner blocSpinner = (Spinner) alertView.findViewById(R.id.blocSpinner);
+        blocSpinner.setAdapter(adapter);
+
+        locationAlert.setView(alertView);
+        locationAlert.setPositiveButton(R.string.textConfirm, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                String location = blocSpinner.getSelectedItem().toString();
+                if (location.equals("Public")) {
+                    locationBtn.setImageResource(R.drawable.public_note_icon);
+                    bloc = null;
+                } else {
+                    //get bloc of user
+                }
+            }
+        });
+
+        locationAlert.setNegativeButton(R.string.textCancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                //do nothing
+            }
+        });
+
+        locationAlert.show();
     }
 }
