d2b85de0af577b7c365d18f354572e3a935268f3
ebackers <arcestalavera@gmail.com>
2016-03-14 14:58:06 +0800

    Mini Challenge 2 - Project Start

---
+++ Mini_Challenge_2-UnitConversion/app/src/main/java/com/example/arces/mini_challenge_2_unitconversion/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.arces.mini_challenge_2_unitconversion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
